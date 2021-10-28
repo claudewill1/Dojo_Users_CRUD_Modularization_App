b0eb736a4b0b72daf97380be3157a2878269f161
Claude Will <claude.will@yahoo.com>
2021-10-28 01:16:16 -0500

    Users CRUD App"

---
+++ models/user.py
@@ -0,0 +1,49 @@
+from mysqlconnection import connectToMySQL
+
+class User:
+    db_name = "users"
+    def __init__(self,data) -> None:
+        self.id = data['id']
+        self.first_name = data['first_name']
+        self.last_name = data['last_name']
+        self.email = data['email']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+
+    # Now we use class methods to query our database
+    @classmethod
+    def get_all(cls):
+        query = "SELECT * FROM users;"
+        # make sure to call the connectToMySQL function with the schema you are targeting
+        results = connectToMySQL(cls.db_name).query_db(query)
+        # create an empty list to append our instances of users
+        users = []
+        # Iterate over the db results and create instances of users with cls.
+        for user in results:
+            users.append(cls(user))
+        return users
+    
+    @classmethod
+    def getOne(cls,data):
+        query = "SELECT * FROM users WHERE id = %(id)s;"
+        result = connectToMySQL(cls.db_name).query_db(query,data)
+        return cls(result[0])
+
+    # class method to add a friend to the database
+    @classmethod
+    def save(cls,data):
+        query = "INSERT INTO users (first_name,last_name,email,created_at,updated_at) VALUES (%(first_name)s,%(last_name)s, %(email)s, NOW(), NOW());"
+        # data is a dictionary that will be passed into the save method from server.py
+        return connectToMySQL(cls.db_name).query_db(query,data)
+
+    # class method to update a user in the database
+    @classmethod
+    def update(cls,data):
+        query = "UPDATE users SET first_name=%(first_name)s, last_name=%(last_name)s, email=%(email)s WHERE id = %(id)s;"
+        return connectToMySQL(cls.db_name).query_db(query,data)
+
+    # class method to delete user from database
+    @classmethod
+    def delete(cls,data):
+        query = "DELETE FROM users WHERE id = %(id)s;"
+        return connectToMySQL(cls.db_name).query_db(query,data)
